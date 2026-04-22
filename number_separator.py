a2911eb91cc6068ad2f1cb897e48dc5dd478b18d
Ryan-Nening <neningryan10@gmail.com>
2026-04-23 01:40:52 +0800

    I added the init method to set up the main window properties.

--- number_separator.py
+++ number_separator.py
@@ -2,3 +2,7 @@ import tkinter
 from tkinter import filedialog
 from tkinter import messagebox
 
+def __init__(self):
+        self.main_window = tkinter.Tk()
+        self.main_window.title("Number Separator Tool")
+        self.main_window.geometry("300x150")
